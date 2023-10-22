1f51ef3d200b19a2541bd8cbcdee8c89aeed5d83
eldar <eldar.zaripoff@gmail.com>
2023-10-23 00:05:19 +0300

    Correct String

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -11,6 +11,10 @@ import javax.servlet.http.HttpServletResponse;
 public class MainServlet extends HttpServlet {
   private PostController controller;
   private static final String API_POSTS_PATH = "/api/posts";
+  private static final String ANOTHER_API_POSTS_PATH = API_POSTS_PATH + "/\\d+";
+  private static final String GET = "GET";
+  private static final String POST = "POST";
+  private static final String DELETE = "DELETE";
 
   @Override
   public void init() {
@@ -26,21 +30,21 @@ public class MainServlet extends HttpServlet {
       final var path = req.getRequestURI();
       final var method = req.getMethod();
       // primitive routing
-      if (method.equals("GET") && path.equals(API_POSTS_PATH)) {
+      if (method.equals(GET) && path.equals(API_POSTS_PATH)) {
         controller.all(resp);
         return;
       }
       long l = Long.parseLong(path.substring(path.lastIndexOf("/")));
-      if (method.equals("GET") && path.matches(API_POSTS_PATH + "/\\d+")) {
+      if (method.equals(GET) && path.matches(ANOTHER_API_POSTS_PATH)) {
         // easy way
         controller.getById(l, resp);
         return;
       }
-      if (method.equals("POST") && path.equals(API_POSTS_PATH)) {
+      if (method.equals(POST) && path.equals(API_POSTS_PATH)) {
         controller.save(req.getReader(), resp);
         return;
       }
-      if (method.equals("DELETE") && path.matches(API_POSTS_PATH + "/\\d+")) {
+      if (method.equals(DELETE) && path.matches(ANOTHER_API_POSTS_PATH)) {
         // easy way
         controller.removeById(l, resp);
         return;
